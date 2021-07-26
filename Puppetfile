1ba6913225755ffc821422f9e9a6d3c1feb639c4
root <root@puppet.codespaces.io>
2021-07-26 14:27:31 +0000

    importing node data:

--- Puppetfile
+++ Puppetfile
@@ -2,16 +2,16 @@ forge 'https://forge.puppet.com'
 
 # Modules from the Puppet Forge
 # Versions should be updated to be the latest at the time you start
-#mod 'puppetlabs/inifile', '5.0.1'
-#mod 'puppetlabs/stdlib',  '7.0.1'
+mod 'puppetlabs/ntp', '4.2.0'
+mod 'puppetlabs/haproxy',  '1.5.0'
 #mod 'puppetlabs/concat',  '7.0.1'
 
 # Modules from Git
 # Examples: https://github.com/puppetlabs/r10k/blob/master/doc/puppetfile.mkd#examples
-#mod 'apache',
-#  git:    'https://github.com/puppetlabs/puppetlabs-apache',
+mod 'tomcat',
+  git:    'https://github.com/EDockus/puppet-module-tomcat.git',
 #  commit: '1b6f89afdde0df7f9433a163d5c4b5328eac5779'
 
-#mod 'apache',
-#  git:    'https://github.com/puppetlabs/puppetlabs-apache',
+mod 'java',
+  git:    'https://github.com/EDockus/puppet-module-java.git',
 #  branch: 'docs_experiment'
---
+++ manifests/app.pp
@@ -0,0 +1,46 @@
+node 'node1' {
+    notify {"***** NODE1 BLOCK *****":}
+    include java::install, base
+    
+        class{'::tomcat':
+        user  => 'tomcat',
+        group => 'tomcat',
+        xms   => '60m',
+        xmx   => '120m'
+    }
+    
+    tomcat::deploy { "sysfoo":
+        deploy_url     => 'puppet:///modules/tomcat/sysfoo.war',
+        checksum_value => 'c808ccfcedfd7434993ddd15e14d8e4a'
+    }
+}
+
+node 'node2' {
+    notify {"***** NODE2 BLOCK *****":}
+    include java::install, base
+    
+    class{'::tomcat':
+        user  => 'tomcat',
+        group => 'tomcat',
+        xms   => '64m',
+        xmx   => '128m'
+    }
+    
+    tomcat::deploy { "sysfoo":
+        deploy_url => 'puppet:///modules/tomcat/sysfoo.war',
+        checksum_value => 'c808ccfcedfd7434993ddd15e14d8e4a'
+    }
+}
+
+node default {
+    notify{"checkpoint_1" :
+        message => "
+        
+            CHECKPOINT_1
+            
+            Applying Default BLOCK.
+            The node has connected to the Puppet Master correctly, however, there are no definitions for it configured at this point.
+            
+        "    
+    }
+}
